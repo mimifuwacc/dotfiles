7a611d8752235cb9b5686c34c80261011d772033
mimifuwa <mimifuwa.cc@gmail.com>
2025-10-17 19:18:05 +0900

    chore(wezterm): テーマ設定を別ファイルに切り分け

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -31,37 +31,13 @@ config.show_new_tab_button_in_tab_bar = false
 -- only for nightly builds
 config.show_close_tab_button_in_tabs = false
 
-config.colors = {
-  foreground = '#e1e4e8',
-  ansi = {
-    '#484f58',
-    '#ff7b72',
-    '#3fb950',
-    '#d29922',
-    '#58a6ff',
-    '#bc8cff',
-    '#39c5cf',
-    '#b1bac4',
-  },
-  brights = {
-    '#6e7681',
-    '#ffa198',
-    '#56d364',
-    '#e3b341',
-    '#79c0ff',
-    '#d2a8ff',
-    '#56d4dd',
-    '#ffffff',
-  },
-  background = '#22272e', 
-  cursor_bg = '#58a6ff',
-  cursor_fg = '#e1e4e8',
-  cursor_border = '#58a6ff',
-  selection_fg = '#e1e4e8',
-  selection_bg = '#223244',
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
+-- Load theme
+local theme = require 'themes.github-dark'
+config.colors = theme
+
+-- Tab bar configuration
+config.colors.tab_bar = {
+  inactive_tab_edge = "none",
 }
 
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
