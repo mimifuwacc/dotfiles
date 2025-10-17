615c0d5e149c61314e4c1414d7871b067f7c0fb4
mimifuwacc <kimimichi_shioiri@dwango.co.jp>
2025-10-17 19:01:58 +0900

    feat: weztermを追加

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,98 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.automatically_reload_config = true
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font_size = 20
+config.font = wezterm.font_with_fallback {
+  { family = "Calex Code JP", weight = "Bold" },
+}
+config.use_ime = true
+config.window_decorations = "RESIZE"
+
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+  font_size = 20.0,
+  font = wezterm.font { family = "Calex Code JP", weight = "Bold" },
+}
+
+config.window_background_gradient = {
+  colors = { "#1F2428" },
+}
+
+config.show_new_tab_button_in_tab_bar = false
+
+-- only for nightly builds
+config.show_close_tab_button_in_tabs = false
+
+config.colors = {
+  foreground = '#e1e4e8',
+  ansi = {
+    '#484f58',
+    '#ff7b72',
+    '#3fb950',
+    '#d29922',
+    '#58a6ff',
+    '#bc8cff',
+    '#39c5cf',
+    '#b1bac4',
+  },
+  brights = {
+    '#6e7681',
+    '#ffa198',
+    '#56d364',
+    '#e3b341',
+    '#79c0ff',
+    '#d2a8ff',
+    '#56d4dd',
+    '#ffffff',
+  },
+  background = '#22272e', 
+  cursor_bg = '#58a6ff',
+  cursor_fg = '#e1e4e8',
+  cursor_border = '#58a6ff',
+  selection_fg = '#e1e4e8',
+  selection_bg = '#223244',
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_right_half_circle_thick
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#22272e"
+  local foreground = "#e1e4e8"
+  local edge_background = "none"
+
+  if tab.is_active then
+    background = "#ec775c"
+    foreground = "#e1e4e8"
+  end
+
+  local edge_foreground = background
+
+  local title = "  " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "  "
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+ end)
+
+-- and finally, return the configuration to wezterm
+return config
