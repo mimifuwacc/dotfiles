eb5a533910daaff5af9d53a2610575f9a8943858
mimifuwacc <mimifuwa.cc@gmail.com>
2026-01-15 12:34:26 +0900

    feat: lazyvimの設定など

--- dot_config/nvim/lua/keymaps.lua
+++
@@ -1,8 +0,0 @@
-for k, v in pairs({
-    ['vv'] = '<S-v>',
-    ['<F2>'] = ':FzfLua files<CR>',
-    [':f'] = ':FzfLua ',
-    ['<F3>'] = ':Yazi<CR>',
-}) do 
-    vim.api.nvim_set_keymap('n', k, v, {})
-end
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -9,21 +9,21 @@ config.automatically_reload_config = true
 
 -- For example, changing the color scheme:
 config.font_size = 20
-config.font = wezterm.font_with_fallback {
-  { family = "Calex Code JP", weight = "Bold" },
-}
+config.font = wezterm.font_with_fallback({
+	{ family = "Calex Code JP" },
+})
 config.use_ime = true
 config.window_decorations = "RESIZE"
 
 config.window_frame = {
-  inactive_titlebar_bg = "none",
-  active_titlebar_bg = "none",
-  font_size = 20.0,
-  font = wezterm.font { family = "Calex Code JP", weight = "Bold" },
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+	font_size = 20.0,
+	font = wezterm.font({ family = "Calex Code JP", weight = "Bold" }),
 }
 
 config.window_background_gradient = {
-  colors = { "#1F2428" },
+	colors = { "#1F2428" },
 }
 
 config.show_new_tab_button_in_tab_bar = false
@@ -32,43 +32,44 @@ config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 
 -- Load theme
-local theme = require 'themes.github-dark'
+local theme = require("themes.github-dark")
 config.colors = theme
 
 -- Tab bar configuration
 config.colors.tab_bar = {
-  inactive_tab_edge = "none",
+	inactive_tab_edge = "none",
 }
 
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_right_half_circle_thick
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#22272e"
-  local foreground = "#e1e4e8"
-  local edge_background = "none"
+	local background = "#22272e"
+	local foreground = "#e1e4e8"
+	local edge_background = "none"
 
-  if tab.is_active then
-    background = "#ec775c"
-    foreground = "#e1e4e8"
-  end
+	if tab.is_active then
+		background = "#ec775c"
+		foreground = "#e1e4e8"
+	end
 
-  local edge_foreground = background
+	local edge_foreground = background
 
-  local title = "  " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "  "
+	local title = "  " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "  "
 
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
- end)
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
 
 -- and finally, return the configuration to wezterm
 return config
+
