d25dc86253b61262f6e0e735ae81233e42620215
mimifuwacc <mail@mimifuwa.cc>
2026-02-22 21:59:38 +0900

    chore(zsh): sheldonをクビにした

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -14,7 +14,6 @@
     pkgs.google-chrome
     pkgs.vscode
     pkgs.discord
-    pkgs.sheldon
     pkgs.starship
     pkgs.eza
     pkgs.shottr
@@ -28,18 +27,71 @@
     pkgs.fastfetch
     pkgs.claude-code
     pkgs.orbstack
+    pkgs.zsh-defer
+    pkgs.zsh-syntax-highlighting
+    pkgs.zsh-autosuggestions
   ];
 
   home.file = {
     "Taskfile.yaml".source = dotfilesPath "Taskfile.yaml";
-    ".zshrc".source = dotfilesPath "zsh/.zshrc";
     ".config/discord".source = dotfilesPath "discord";
     ".config/wezterm".source = dotfilesPath "wezterm";
-    ".config/sheldon".source = dotfilesPath "sheldon";
     ".config/starship.toml".source = dotfilesPath "starship/starship.toml";
     ".gitconfig".source = dotfilesPath "git/darwin/.gitconfig";
   };
 
   targets.darwin.linkApps.enable = true;
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    history = {
+      size = 100000;
+      save = 100000;
+      path = "$HOME/.zsh_history";
+      share = true;
+      extended = true;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      expireDuplicatesFirst = true;
+      saveNoDups = true;
+    };
+
+    historySubstringSearch = {
+      enable = true;
+      searchUpKey = ["^P"];
+      searchDownKey = ["^N"];
+    };
+
+    shellAliases = {
+      ls = "eza --icons --git --time-style relative";
+      vim = "nvim";
+      ssh = "TERM=xterm ssh";
+      ccusage = "bunx ccusage@latest";
+    };
+
+    initContent = ''
+      # Environment variables
+      export _USERNAME=$(whoami)
+      export _HOSTNAME=$(hostname -s)
+
+      # Starship prompt
+      if type starship &>/dev/null; then
+        eval "$(starship init zsh)"
+      fi
+
+      # Direnv
+      if type direnv &>/dev/null; then
+        eval "$(direnv hook zsh)"
+      fi
+
+      # Plugins with zsh-defer
+      source ${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh
+      zsh-defer ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+      zsh-defer ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    '';
+  };
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,60 +1,45 @@
 export _USERNAME=$(whoami)
 export _HOSTNAME=$(hostname -s)
 
-# Load zsh plugins from sheldon
-if type sheldon &>/dev/null; then
-  eval "$(sheldon source)"
-fi
-
 # Load starship prompt
 if type starship &>/dev/null; then
   eval "$(starship init zsh)"
 fi
 
-# Set eza for default ls
-if type eza &>/dev/null; then
-  alias ls='eza --icons --git --time-style relative'
-fi
-
-# Add nvim alias
-if type nvim &>/dev/null; then
-  alias vim=nvim
-  alias v=nvim
+# Load direnv
+if type direnv &>/dev/null; then
+  eval "$(direnv hook zsh)"
 fi
 
 # History
 export HISTFILE=${HOME}/.zsh_history
 export HISTSIZE=1000
 export SAVEHIST=100000
+
+# History key bindings
 autoload -Uz history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^P" history-beginning-search-backward-end
 bindkey "^N" history-beginning-search-forward-end
 
-setopt hist_ignore_dups
-setopt inc_append_history
-setopt share_history
-setopt hist_ignore_all_dups
-setopt hist_save_no_dups
-setopt hist_expire_dups_first
+# zsh options
+setopt mark_dirs auto_param_slash auto_cd auto_menu correct
+setopt interactive_comments magic_equal_subst complete_in_word
+setopt print_eight_bit no_beep
+setopt hist_ignore_dups inc_append_history share_history
+setopt hist_ignore_all_dups hist_save_no_dups hist_expire_dups_first
 setopt extended_history
 
-# Other options
-setopt mark_dirs
-setopt auto_param_slash
-setopt auto_cd
-setopt auto_menu
-setopt correct
-setopt interactive_comments
-setopt magic_equal_subst
-setopt complete_in_word
-setopt print_eight_bit
-setopt no_beep
-
-# SSH
+# Aliases
+alias ls='eza --icons --git --time-style relative'
+alias vim=nvim
+alias v=nvim
 alias ssh="TERM=xterm ssh"
-
-# Other aliases
 alias a=./a.out
 alias ccusage="bunx ccusage@latest"
+
+# direnv alias
+if type direnv &>/dev/null; then
+  alias nix-direnv="echo 'use nix' >> .envrc && direnv allow"
+fi
